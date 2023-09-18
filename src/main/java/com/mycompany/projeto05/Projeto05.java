7f8a80aa834f92a5f9545d308271e34e0e5cde95
TA�ARA FERNANDA SANCHES DIAS <tayara.5970@aluno.pr.senac.br>
2023-09-18 15:57:41 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/projeto05/Projeto05.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto05;
+
+/**
+ *
+ * @author tayara.5970
+ */
+public class Projeto05 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
